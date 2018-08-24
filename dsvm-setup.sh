8277e0f544e1412ba68c6a42eef537822a7e327c
Jeremy Reynolds <jeremy.reynolds@microsoft.com>
2018-08-24 11:33:32 -0600

    update instructions and never overwrite files

--- dsvm-setup.sh
+++ dsvm-setup.sh
@@ -3,20 +3,35 @@
 # This should set up an Ubuntu DSVM for this course
 # Assumes you've deployed an NC-series VM, so CUDA
 # is already installed in the py35 conda env
-
 # this is based on the last part of 
 # http://files.fast.ai/setup/paperspace
 
-## clone repo from github into the directory
-## 
+# It really just does two things.
+# 1. makes sure the fastai repo is cloned in the ~/notebooks directory so that
+#    jupyter sees it by default
+# 2. makes sure the data is downloaded in the ~/notebooks/data directory, and linked inside fastai/courses/dl1
+
+## Usage approach number 1:
+## You can use this files via direct download:
+## wget https://raw.githubusercontent.com/jreynolds01/fastai/dsvm-setup/dsvm-setup.sh
+## chmod u+x dsvm-setup.sh
+## ./dsvm-setup.sh
+
+## Usage approach number 2:
+## Clone and run from github using git
+## logged into the DSVM:
+## cd ~/notebooks
+## just using this because it's in a personal branch
+## git clone https://github.com/jreynolds01/fastai.git
+## git checkout dsvm-setup
+## ./fastai/dsvm-setup.sh
+
 WD=~/notebooks
 
-## activate appropriate conda env
+## activate appropriate conda env in case we need to add any pip or conda installs below
 source activate py35
 
-
 cd $WD
-
 echo "Working in `pwd`"
 
 if [ -d "fastai" ]; then
@@ -34,15 +49,23 @@ fi
 
 # download the dogscats data!
 # do this...
-
 cd data
-echo "downloading data..."
-wget http://files.fast.ai/data/dogscats.zip
+if [ -f dogscats.zip ]; then
+    echo "dogscats already exists!"
+else
+    echo "downloading data..."
+    wget http://files.fast.ai/data/dogscats.zip
+fi
+
 echo "unzipping data..."
-unzip -q dogscats.zip
-cd ${WD}/fastai/courses/dl1/
+## never overwrite files
+unzip -qn dogscats.zip
 ## link data dir in the dl1 course.
+cd ${WD}/fastai/courses/dl1/
 ln -s ~/data ./
+
+## clean up zipfile
+/bin/rm dogscats.zip
 
 echo "Done!"
 # not sure utility of this - will come back after walking through course...
